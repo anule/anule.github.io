7f8635ecbc740d8c1a008ee287a0d68339949bc3
anule <anule.ndukwu@gmail.com>
2018-02-13 12:56:29 -0500

    add sticky header

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,19 +2,25 @@ import React from 'react';
 import './Navbar.css';
 
 const Navbar = () => (
-  <nav className="navbar" role="navigation" aria-label="main navigation">
-    <h1 className="navbar-item">Anule Ndukwu</h1>
+  <nav
+    className="navbar is-fixed-top"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div id="nav-items">
+      <h1 className="navbar-item">Anule Ndukwu</h1>
 
-    <div className="navbar-menu">
-      <span className="navbar-item">
-        <a href="#about">About Me</a>
-      </span>
-      <span className="navbar-item">
-        <a href="#projects">Projects</a>
-      </span>
-      <span className="navbar-item">
-        <a href="#contact">Contact</a>
-      </span>
+      <div className="navbar-menu">
+        <span className="navbar-item">
+          <a href="#about">About Me</a>
+        </span>
+        <span className="navbar-item">
+          <a href="#projects">Projects</a>
+        </span>
+        <span className="navbar-item">
+          <a href="#contact">Contact</a>
+        </span>
+      </div>
     </div>
   </nav>
 );
