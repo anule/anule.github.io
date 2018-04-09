7343aa3403983d9bee28781e47862a73b4e070a8
anule <anule.ndukwu@gmail.com>
2018-04-09 11:06:39 -0400

    add git twitter bot

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,31 +3,31 @@ import './Navbar.css';
 import Contact from './Contact';
 
 const Navbar = () => (
-  <header>
-    <nav
-      className="navbar is-fixed-top"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div id="nav-items">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="https://anule.github.com">
-            <h1>Anule Ndukwu</h1>
-          </a>
-          <div>
-            <span>
-              <a href="#about">About Me</a>
-            </span>
-            <span>
-              <a href="#projects">Projects</a>
-            </span>
-            <span>
-              <a href="#contact">Contact</a>
-            </span>
-          </div>
-        </div>
+    <header>
+        <nav
+            className="navbar is-fixed-top"
+            role="navigation"
+            aria-label="main navigation"
+        >
+            <div id="nav-items">
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="https://anule.github.com">
+                        <h1>Anule Ndukwu</h1>
+                    </a>
+                    <div>
+                        <span>
+                            <a href="#about">About Me</a>
+                        </span>
+                        <span>
+                            <a href="#projects">Projects</a>
+                        </span>
+                        <span>
+                            <a href="#contact">Contact</a>
+                        </span>
+                    </div>
+                </div>
 
-        {/*<div className="navbar-menu">
+                {/*<div className="navbar-menu">
           <span className="navbar-item">
             <a href="#about">About Me</a>
           </span>
@@ -38,10 +38,10 @@ const Navbar = () => (
             <a href="#contact">Contact</a>
           </span>
 </div>*/}
-      </div>
-    </nav>
-    <Contact />
-  </header>
+            </div>
+        </nav>
+        <Contact />
+    </header>
 );
 
 export default Navbar;
--- src/Projects.js
+++ src/Projects.js
@@ -6,43 +6,43 @@ import projects from './info/projects.json';
 // import neo4j from './images/neo4j-display.png';
 
 const Projects = () => (
-  <section id="projects">
-    <h1 className="title">projects</h1>
-    <div className="project-tiles">
-      {projects.map(project => (
-        <div className="project" key={project.title}>
-          <h1 className="project-title">{project.title}</h1>
-          <div className="project-img">
-            <img src={project.image} alt={project.imageAlt} />
-          </div>
-          <div className="buttons">
-            <a href={project.demo}>
-              <button className="button is-medium is-outlined demo" role="link">
+    <section id="projects">
+        <h1 className="title">projects</h1>
+        <div className="project-tiles">
+            {projects.map(project => (
+                <div className="project" key={project.title}>
+                    <h1 className="project-title">{project.title}</h1>
+                    <div className="project-img">
+                        <img src={project.image} alt={project.imageAlt} />
+                    </div>
+                    <div className="buttons">
+                        <a href={project.demo}>
+                            <button className="button is-medium is-outlined demo" role="link">
                 Demo
-              </button>
-            </a>
-            <a href={project.code}>
-              <button
-                className="button is-medium is-outlined GitHub"
-                role="link"
-              >
+                            </button>
+                        </a>
+                        <a href={project.code}>
+                            <button
+                                className="button is-medium is-outlined GitHub"
+                                role="link"
+                            >
                 Code
-              </button>
-            </a>
-          </div>
-          <div className="description">
-            <span className="text">{project.timeSpan}</span>
-            <br />
-            <p className="text">{project.description}</p>
-            <br />
-            <p className="text">
-              <b>Tech stack</b>: {project.stack}
-            </p>
-          </div>
+                            </button>
+                        </a>
+                    </div>
+                    <div className="description">
+                        <span className="text">{project.timeSpan}</span>
+                        <br />
+                        <p className="text">{project.description}</p>
+                        <br />
+                        <p className="text">
+                            <b>Tech stack</b>: {project.stack}
+                        </p>
+                    </div>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  </section>
+    </section>
 );
 
 export default Projects;
