38997d27866cf9798a25af5c4121feb52ce6aa7a
anule <anule.ndukwu@gmail.com>
2018-10-08 20:13:59 -0700

    bleh

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,27 +2,24 @@ import React from 'react';
 import './Navbar.css';
 
 const Navbar = () => (
-    <nav
-        className="navbar is-fixed-top"
-        role="navigation"
-        aria-label="main navigation"
-    >
-        <div id="nav-items">
-            <h1 className="navbar-item">Anule Ndukwu</h1>
-
-            <div className="navbar-menu">
-                <span className="navbar-item">
-                    <a href="#about">About Me</a>
-                </span>
-                <span className="navbar-item">
-                    <a href="#projects">Projects</a>
-                </span>
-                <span className="navbar-item">
-                    <a href="#contact">Contact</a>
-                </span>
-            </div>
-        </div>
-    </nav>
+  <nav
+    className="navbar is-fixed-top"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div id="nav-items">
+      <h1 className="navbar-item">Anule Ndukwu</h1>
+      <span className="navbar-item">
+        <a href="#about">About Me</a>
+      </span>
+      <span className="navbar-item">
+        <a href="#projects">Projects</a>
+      </span>
+      <span className="navbar-item">
+        <a href="#contact">Contact</a>
+      </span>
+    </div>
+  </nav>
 );
 
 export default Navbar;
