ec880e3fc20dfaf3c2127498d074a0f0645da4c6
anule <anule.ndukwu@gmail.com>
2019-12-11 23:05:17 -0600

    Updates

---
+++ precache-manifest.5a2f0e9bd02ea0ca2e4484d716be0ed2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f96cfa57fcda3479d7f483887fd458b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99918c466e6b7c2260f8",
+    "url": "/static/css/main.cb82806e.chunk.css"
+  },
+  {
+    "revision": "a67af0e8f054f34bbd9f",
+    "url": "/static/js/2.c7c664dc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c7c664dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99918c466e6b7c2260f8",
+    "url": "/static/js/main.98e1398a.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  },
+  {
+    "revision": "00dea9009671cf2b5e47341a6a9289b9",
+    "url": "/static/media/global-kitchen.00dea900.png"
+  },
+  {
+    "revision": "47ca20a86f342a4913edecd988771a85",
+    "url": "/static/media/h2store.47ca20a8.png"
+  },
+  {
+    "revision": "34d6e6bd0fbb938c3955f0cd42fc7512",
+    "url": "/static/media/neo4j-display.34d6e6bd.png"
+  },
+  {
+    "revision": "7d1e44754feb678191e292ac0933bfe5",
+    "url": "/static/media/profile-pic.7d1e4475.jpg"
+  }
+]);
